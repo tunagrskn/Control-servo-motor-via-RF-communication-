e41a54b5e08cd0fd9b7932c7ea860a9bc00e1845
tunagrskn <90112094+tunagrskn@users.noreply.github.com>
2022-03-18 23:23:24 +0300

    Add files via upload

---
+++ Verici.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include "RF24.h"
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+  
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+  
+int derece[1];
+  
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+void setup(void){ 
+Wire.begin();
+mpu.initialize();
+Serial.begin(9600);
+radio.begin();
+radio.openWritingPipe(pipe);
+}
+void loop(void){ 
+ mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+derece[0] = map(ax, 0, 17000, 0,179);
+ radio.write(derece, 1);
+ Serial.println(derece[0]);
+ delay(500);
+ 
+}
